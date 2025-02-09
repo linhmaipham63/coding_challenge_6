bd0e5e12e802265d2772c5345b7221f00b83491b
Linh Pham <linhmaipham@usf.edu>
2025-02-09 17:56:28 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -10,3 +10,16 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 calculateProfit(20, 30, 100); 
 calculateProfit(50, 70, 200); 
 
+
+// Task 2: Function Expression
+
+// Declare function expression calculateSalesTax(amount, taxRate)
+let calculateSalesTax = function(amount, taxRate) {
+    let salesTax =  amount*taxRate;
+    console.log(`Sales Tax: $${Math.round(salesTax)}`);
+}
+
+// Test Data:
+calculateSalesTax(100, 0.07); 
+calculateSalesTax(500, 0.1);  
+

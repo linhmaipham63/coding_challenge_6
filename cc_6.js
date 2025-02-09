067174ea45f8174181dc61ced39c89adbae6653c
Linh Pham <linhmaipham@usf.edu>
2025-02-09 17:57:08 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -23,3 +23,23 @@ let calculateSalesTax = function(amount, taxRate) {
 calculateSalesTax(100, 0.07); 
 calculateSalesTax(500, 0.1);  
 
+
+// Task 3: Arrow Function
+
+// Write arrow function calculateBonus(salary, performanceRating)
+calculateBonus = (salary, performanceRating) => {
+    let bonus; 
+    if (performanceRating === "Excellent") {
+        bonus = salary * 0.2; // "Excellent" → 20% of salary bonus
+    } else if (performanceRating === "Good") {
+        bonus = salary * 0.1; // "Good" → 10% of salary bonus
+    } else if (performanceRating === "Average") {
+        bonus = salary * 0.05; // "Average" → 5% of salary bonus
+    }
+    console.log(`Bonus: $${bonus}`);
+}
+
+// Test Data:
+calculateBonus(5000, "Excellent"); 
+calculateBonus(7000, "Good");      
+
